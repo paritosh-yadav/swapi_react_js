7b0fe47d6f61ed54123e32bdba33f84c9688c4f8
Paritosh Yadav <paritosh.yadav.237@gmail.com>
2018-12-27 15:25:07 +0530

    redux and routing added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger';
+import rootReducer from './redux/';
+
 import './index.css';
-import App from './App';
+import LoginContainer from "./views/login/login";
+import LandingPageContainer from './views/landing-page/LandingPageContainer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, createLogger()));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <div>
+                <Route path="/" exact component={LoginContainer} />
+                <Route path="/landing/" component={LandingPageContainer} />
+            </div>
+        </Router>
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
